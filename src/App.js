0d2e3a6b8fc975e652fc8535b05bf61777679bad
veer <chandraveerdude@gmail.com>
2023-09-18 02:59:57 +0530

    successfully linked yaml pipeline to dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 // import './App.css';
 
 function App() {
+  const handleClick = async () => {
+    try {
+      const response = await fetch('https://api.github.com/repos/chandraveermathur/DASHBOARD-YAML/actions/workflows/main.yml/dispatches', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/vnd.github.v3+json',
+          'Authorization': 'token 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'
+        },
+        body: JSON.stringify({
+          ref: 'main'
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>My name is Veer</h2>
+      <button onClick={handleClick}>Trigger</button>
     </div>
   );
 }
